bcb6fca651eec7490666318ca710f67618fb97e3
ivancassiano <ivancassyano@hotmail.com>
2025-11-23 00:39:47 -0300

    Add PostgreSQL database integration for product listing
    Replaced hardcoded product data with dynamic fetching from the database. Updated `Produto` struct and added database connection logic. Included dependency for `github.com/lib/pq`.

---
+++ go.mod
@@ -0,0 +1,5 @@
+module curso_golang_alura
+
+go 1.25
+
+require github.com/lib/pq v1.10.9
--- main.go
+++ main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"database/sql"
+	_ "github.com/lib/pq"
 	"html/template"
 	"net/http"
 )
 
+func conectaComBancoDeDados() *sql.DB {
+	conexao := "user=cassiano dbname=alura_loja password=cassiano host=localhost port=5432 sslmode=disable"
+	db, err := sql.Open("postgres", conexao)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return db
+}
+
 type Produto struct {
 	Nome, Descricao string
 	Preco           float64
-	Quantidade      int
+	Id, Quantidade  int
 }
 
 var temp = template.Must(template.ParseGlob("templates/*.html"))
@@ -19,11 +31,35 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	produtos := []Produto{
-		{Nome: "Camiseta", Descricao: "Azul, bem bonita", Preco: 39, Quantidade: 5},
-		{"Tenis", "Confortável", 89, 3},
-		{"Fone", "Muito bom", 59, 2},
-		{"Produto novo", "Muito legal", 1.99, 1},
+	db := conectaComBancoDeDados()
+
+	selectDeTodosOsProdutos, err := db.Query("SELECT * FROM produtos")
+	if err != nil {
+		panic(err.Error())
 	}
+
+	p := Produto{}
+	produtos := []Produto{}
+	for selectDeTodosOsProdutos.Next() {
+		var id, quantidade int
+		var nome, descricao string
+		var preco float64
+
+		err = selectDeTodosOsProdutos.Scan(&id, &nome, &descricao, &preco, &quantidade)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		p.Nome = nome
+		p.Id = id
+		p.Descricao = descricao
+		p.Preco = preco
+		p.Quantidade = quantidade
+
+		produtos = append(produtos, p)
+	}
+
+	defer db.Close()
+
 	temp.ExecuteTemplate(w, "Index", produtos)
 }
